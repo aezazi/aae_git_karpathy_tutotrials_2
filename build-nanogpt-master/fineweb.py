3d0282f6a372750078359ab4ca2c7de77ff57d39
aezazi <aezazi@icloud.com>
2025-06-03 12:03:55 +0000

    Karpathy fineweb.py ran on Lambda labs. Dataset down loaded. Added the sharded and tokenized data to gitignore

--- build-nanogpt-master/fineweb.py
+++ build-nanogpt-master/fineweb.py
@@ -87,3 +87,7 @@ with mp.Pool(nprocs) as pool:
         write_datafile(filename, all_tokens_np[:token_count])
 
 # %%
+tokens = np.fromfile("edu_fineweb10B/edufineweb_train_000013.npy", dtype=np.uint16)
+print(tokens[:-10])
+print(len(tokens))
+# %%
