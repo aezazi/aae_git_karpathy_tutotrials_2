9759680c7664cb380fc1aedd58c91566d6b7e96b
Alex Ezazi <aezazi@niteo.us>
2025-06-08 09:58:37 -0400

    experiments with using numpy arrays to gain efficiency

--- build-nanogpt-master/aae_fineweb_multiprocess.py
+++ build-nanogpt-master/aae_fineweb_multiprocess.py
@@ -26,18 +26,25 @@ num_workers = max(1, mp.cpu_count()-1)
 os.makedirs(shard_dir, exist_ok=True)
 
 #%%
-encoder = tiktoken.get_encoding(tokenizer_name)
-eot_token_id = encoder.eot_token # get gpt2 tokenizer eot token id
-print(eot_token_id)
+def create_encoder():
+    encoder = tiktoken.get_encoding(tokenizer_name)
+    eot_token_id = encoder.eot_token # get gpt2 tokenizer eot token id
+    print(eot_token_id)
+    return encoder, eot_token_id
 #%%
 # Load the dataset with streaming to avoid memory overflow
 
-dataset_iterator = load_dataset("HuggingFaceFW/fineweb-edu", split="train", name="sample-10BT", streaming=False)
+def get_data():
+    dataset_iterator = load_dataset("HuggingFaceFW/fineweb-edu", split="train", name="sample-10BT", streaming=False)
+    return dataset_iterator
+
+
+# dataset_iterator = load_dataset("HuggingFaceFW/fineweb-edu", split="train", name="sample-10BT", streaming=False)
 
 #%%
 # create tokenization function
 # NOTE: The use of encoder.encode_ordinary(). This is beacuse When preparing data for training: You usually want full control over where and when special tokens (like <|endoftext|>) are inserted. encode_ordinary gives you just the raw tokenization, so you can insert special tokens like eot_token_id manually, e.g., at shard boundaries or document ends.
-def tokenize(example: str, eot=eot_token_id):
+def tokenize(example: str, encoder, eot):
     text = example['text']
     tokens = encoder.encode_ordinary(text)
     tokens.append(eot)
@@ -54,59 +61,63 @@ def tokenize(example: str, eot=eot_token_id):
 #     break
 
 # %%
-d_start = time.time()
-with mp.Pool(num_workers) as pool:
-    shard_idx = 0
-    shard_tokens = [eot_token_id] 
-    shard_token_count = len(shard_tokens)
+def create_shards(dataset_iterator, encoder, eot_token_id):
+    d_start = time.time()
 
-    for tokens in pool.map(tokenize, dataset_iterator, chunksize=16):
-        # if adding more tokens goes over the shard size limit, save the shard and start a new shard
-        if shard_token_count + len(tokens) > shard_size:
-            shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:04d}')
-            shard_tokens = np.array(shard_tokens, dtype=np.uint16)
-            np.save(shard_save_path, shard_tokens)
-            
-            # measure time to create this shard
-            d_end = time.time()
-            dt = d_end - d_start
-            d_start = d_end
+    with mp.Pool(num_workers) as pool:
+        shard_idx = 0
+        shard_tokens = [eot_token_id] 
+        shard_token_count = len(shard_tokens)
 
-            print(f'saved to {shard_save_path} with {len(shard_tokens):,} tokens | time to create shard: {dt:.3f}')
+        for tokens in pool.map(tokenize(,encoder, eot_token_id), dataset_iterator, chunksize=16):
+            # if adding more tokens goes over the shard size limit, save the shard and start a new shard
+            if shard_token_count + len(tokens) > shard_size:
+                shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:04d}')
+                shard_tokens = np.array(shard_tokens, dtype=np.uint16)
+                np.save(shard_save_path, shard_tokens)
+                
+                # measure time to create this shard
+                d_end = time.time()
+                dt = d_end - d_start
+                d_start = d_end
 
-            # start new shard
-            shard_idx += 1
-            shard_tokens = [eot_token_id]
-            shard_token_count = len(shard_tokens)
+                print(f'saved to {shard_save_path} with {len(shard_tokens):,} tokens | time to create shard: {dt:.3f}')
 
-        else:
-            shard_tokens.extend(tokens)
-            shard_token_count += len(tokens)
-            print('concating shard len: {shard_token_count}')
+                # start new shard
+                shard_idx += 1
+                shard_tokens = [eot_token_id]
+                shard_token_count = len(shard_tokens)
 
-    if shard_token_count > 0:
-            shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:06d}')
-            np.save(shard_save_path, shard_tokens)
-            
-            d_end = time.time()
-            dt = d_end - d_start
-            print(f'saved to {shard_save_path} with {len(shard_tokens):,} tokens | time to create shard: {dt:.2f} secs')
+            else:
+                shard_tokens.extend(tokens)
+                shard_token_count += len(tokens)
+                print('concating shard len: {shard_token_count:,}')
+
+        if shard_token_count > 0:
+                shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:06d}')
+                np.save(shard_save_path, shard_tokens)
+                
+                d_end = time.time()
+                dt = d_end - d_start
+                print(f'saved to {shard_save_path} with {len(shard_tokens):,} tokens | time to create shard: {dt:.2f} secs')
 
 
 #%%
 # create_shards(dataset_iterator)
 # # %%
-# if __name__ == "__main__":
-#     dataset_iterator = load_dataset("HuggingFaceFW/fineweb-edu", split="train", name="sample-10BT", streaming=False)
-#     create_shards(dataset_iterator)
+if __name__ == "__main__":
+    encoder, eot = create_encoder()
+    
+    dataset_iterator = get_data()
+    create_shards(dataset_iterator, encoder, eot)
 
-# %%
-tokens = np.load("aae_token_shards_multiprocess/shard_000000.npy")
-print(tokens[-1])
-print(len(tokens))
+# # %%
+# tokens = np.load("aae_token_shards_multiprocess/shard_000000.npy")
+# print(tokens[-1])
+# print(len(tokens))
 
-# %%
-a = np.array([1,2,3,4])
-b = np.array([7,8,9])
-print(np.concatenate((a,b), axis=0))
+# # %%
+# a = np.array([1,2,3,4])
+# b = np.array([7,8,9])
+# print(np.concatenate((a,b), axis=0))
 # %%
--- build-nanogpt-master/aae_fineweb_no_multiprocess.py
+++ build-nanogpt-master/aae_fineweb_no_multiprocess.py
@@ -31,17 +31,30 @@ print(eot_token_id)
 # Load the dataset with streaming to avoid memory overflow
 dataset_iterator = load_dataset("HuggingFaceFW/fineweb-edu", split="train", name="sample-10BT", streaming=False)
 
+
+#%%
+dataset_iterator_test = dataset_iterator.select(range(3))
+
 #%%
 # create tokenization function
 # NOTE: The use of encoder.encode_ordinary(). This is beacuse When preparing data for training: You usually want full control over where and when special tokens (like <|endoftext|>) are inserted. encode_ordinary gives you just the raw tokenization, so you can insert special tokens like eot_token_id manually, e.g., at shard boundaries or document ends.
 def tokenize(example: str, eot=eot_token_id):
-    # print('i am here 1')
     text = example['text']
     tokens = encoder.encode_ordinary(text)
     tokens.append(eot)
-    tokens = np.array(tokens)
-    print('tokeninzing')
+    tokens = np.array(tokens, dtype=np.uint16)
+    # print('tokeninzing')
     return tokens
+
+
+#%%
+test = dataset_iterator[0]['text']
+print(test)
+test_tokenize = tokenize(dataset_iterator[0])
+print(test_tokenize)
+print(len(test_tokenize))
+print(len(tokenize(dataset_iterator[1])))
+print(len(tokenize(dataset_iterator[2])))
 
 # # test tokenize function
 # for example in dataset_iterator:
@@ -59,50 +72,57 @@ def tokenize(example: str, eot=eot_token_id):
 def create_shards(dataset_iterator, shard_dir=shard_dir):
     shard_idx = 0
     # initialize shard tokens with eot token at the begining to signal that this document is a continuation
-    shard_tokens = np.array([eot_token_id])
-    shard_token_count = len(shard_tokens)
+    shard_tokens_buffer = np.empty(shard_size, dtype=np.uint16)
+    shard_tokens_buffer[0] = eot_token_id
+    print(f'shard buffer len: {len(shard_tokens_buffer)}')
+    shard_token_count = 1
 
     d_start = time.time()
-    for example in dataset_iterator:
+    for i, example in enumerate(dataset_iterator):
+        
         tokens = tokenize(example)
+        # print(f'shard idx = {shard_idx} | example: {i} | tokens lenght; {len(tokens)}')
 
         # my approach differs from Karpathy in that I do not split documents between shards. If current example does not fit in the current shard, save the shard and start a new one
         if shard_token_count + len(tokens) > shard_size:
             shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:04d}')
             # shard_tokens = np.array(shard_tokens, dtype=np.uint16)
-            np.save(shard_save_path, shard_tokens)
+            shard_tokens_final = shard_tokens_buffer[:shard_token_count].astype(np.uint16)
+            np.save(shard_save_path, shard_tokens_final)
             
             # measure time to create this shard
             d_end = time.time()
             dt = d_end - d_start
             d_start = d_end
 
-            print(f'saved to {shard_save_path} with {len(shard_tokens):,} tokens | time to create shard: {dt:.3f}')
+            print(f'saved to {shard_save_path} with {len(shard_tokens_final):,} tokens | time to create shard: {dt:.3f}')
 
             # start new shard
             shard_idx += 1
-            shard_tokens = np.array([eot_token_id])
-            shard_token_count = len(shard_tokens)
-        
-        
-        shard_tokens = np.concatenate((shard_tokens,tokens))
+            shard_tokens_buffer = np.empty(shard_size)
+            shard_tokens_buffer[0] = eot_token_id
+            shard_token_count = 1
+
+        shard_tokens_buffer[shard_token_count : shard_token_count+len(tokens)] = tokens
         shard_token_count += len(tokens)
-        print(f'conacting | shard length: {shard_token_count}')
+        # print(f'concacting example: {i} to shard: {shard_idx}')
+       
         
     if shard_token_count > 0:
-            shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:06d}')
-            np.save(shard_save_path, shard_tokens)
+            shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:04d}')
+            shard_tokens_final = shard_tokens_buffer[:shard_token_count].astype(np.uint16)
+            np.save(shard_save_path, shard_tokens_final)
             
             d_end = time.time()
             dt = d_end - d_start
-            print(f'saved to {shard_save_path} with {len(shard_tokens):,} tokens | time to create shard: {dt:.2f} secs')
+            print(f'saved to {shard_save_path} with {len(shard_tokens_final):,} tokens | time to create shard: {dt:.2f} secs')
 
 # %%
 create_shards(dataset_iterator)
 # %%
-tokens = np.load("aae_token_shards_no_multiprocess/shard_000001.npy")
+tokens = np.load("aae_token_shards_no_multiprocess/shard_0000.npy")
 print(type(tokens))
-print(tokens[-1])
+print(tokens[0])
 print(len(tokens))
 
 # %%
