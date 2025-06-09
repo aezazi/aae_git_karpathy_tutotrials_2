8ba3555c50559120420db1803428c3ffcb378203
Alex Ezazi <aezazi@niteo.us>
2025-06-09 16:45:29 -0400

    Edited comments

--- build-nanogpt-master/aae_fineweb_multiprocess_M2.py
+++ build-nanogpt-master/aae_fineweb_multiprocess_M2.py
@@ -1,15 +1,10 @@
 #%%
 """
-FineWeb-Edu dataset (for srs pretraining)
-https://huggingface.co/datasets/HuggingFaceFW/fineweb-edu
-Downloads and tokenizes the data and saves data shards to disk.
-Run simply as:
-$ python fineweb.py
-Will save shards to the local directory "edu_fineweb10B".
+
 """
 
 # %%
-
+#imports
 from datasets import load_dataset
 import tiktoken
 import numpy as np
@@ -22,18 +17,14 @@ import multiprocessing as mp
 # Constants
 tokenizer_name = "gpt2"
 shard_size = 100_000_000
-shard_dir = "aae_token_shards_multiprocess_M2"
+shard_dir = "aae_token_shards_mp"
 num_workers = max(1, os.cpu_count()-1)
-
-
 
 #%%
 encoder = tiktoken.get_encoding(tokenizer_name)
 eot_token_id = encoder.eot_token # get gpt2 tokenizer eot token id
-print(eot_token_id)
-#%%
+# print(eot_token_id)
 
-#%%
 # create tokenization function
 # NOTE: The use of encoder.encode_ordinary(). This is beacuse When preparing data for training: You usually want full control over where and when special tokens (like <|endoftext|>) are inserted. encode_ordinary gives you just the raw tokenization, so you can insert special tokens like eot_token_id manually, e.g., at shard boundaries or document ends.
 def tokenize(example, eot=eot_token_id):
@@ -70,13 +61,14 @@ def tokenize(example, eot=eot_token_id):
 def create_shards(dataset_iterator=None, dataset_iterator_test=None, shard_dir=shard_dir):
     os.makedirs(shard_dir, exist_ok=True)
 
+    print(f'num_worker: {num_workers}')
     with mp.Pool(num_workers) as pool:
 
         shard_idx = 0
         # initialize shard tokens with eot token at the begining to signal that this document is a continuation
         shard_tokens_buffer = np.empty(shard_size, dtype=np.uint16)
         shard_tokens_buffer[0] = eot_token_id
-        print(f'shard buffer len: {len(shard_tokens_buffer)}')
+        # print(f'shard buffer len: {len(shard_tokens_buffer)}')
         shard_token_count = 1
 
         d_start = time.time()
@@ -84,19 +76,14 @@ def create_shards(dataset_iterator=None, dataset_iterator_test=None, shard_dir=s
         # NOTE: that pool.map outputs a list of  tokens for each example in dataset_iterator 
         # creating the 'results' variable is how we can wrap the pool output in tqdm for showing progress bars. wraping pool.imap directly in tqdm did not work.
         results = tqdm(pool.imap(tokenize, dataset_iterator), total=len(dataset_iterator), desc=" Percent of datatset processed (cumulative)", unit_scale=True, colour='blue')
-       
-
         
         for tokens  in results:
-            
-            # print(f'shard idx = {shard_idx} | example: {i} | tokens lenght; {len(tokens)}')
-
-            # my approach differs from Karpathy in that I do not split documents between shards. If current example does not fit in the current shard, save the shard and start a new one
             if shard_token_count + len(tokens) > shard_size:
-                shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:04d}')
+                split = 'val' if shard_idx == 0 else 'train'
+                shard_save_path = os.path.join(shard_dir, f'{split}_shard_{shard_idx:04d}')
                 shard_tokens_final = shard_tokens_buffer[:shard_token_count].astype(np.uint16)
                 np.save(shard_save_path, shard_tokens_final)
-                
+
                 # measure time to create this shard
                 d_end = time.time()
                 dt = d_end - d_start
@@ -112,13 +99,6 @@ def create_shards(dataset_iterator=None, dataset_iterator_test=None, shard_dir=s
             else:
                 shard_tokens_buffer[shard_token_count : shard_token_count+len(tokens)] = tokens
                 shard_token_count += len(tokens)
-
-                # if progress_bar is None:
-                #     progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_idx}")
-                # progress_bar.update(len(tokens))
-                # print(f'concacting example: {i} to shard: {shard_idx}')
-                # print(f'shard_token_count: {shard_token_count:,}')
-        
             
         if shard_token_count > 0:
                 shard_save_path = os.path.join(shard_dir, f'shard_{shard_idx:04d}')
@@ -129,8 +109,7 @@ def create_shards(dataset_iterator=None, dataset_iterator_test=None, shard_dir=s
                 dt = d_end - d_start
                 print(f'saved to {shard_save_path} with {len(shard_tokens_final):,} tokens | time to create shard: {dt:.2f} secs')
 
-   
-
+     
 # %%
 if __name__ == '__main__':
     dataset_iterator = load_dataset("HuggingFaceFW/fineweb-edu", split="train", name="sample-10BT", streaming=False)
