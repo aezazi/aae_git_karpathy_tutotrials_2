bug fix evals and experiment with lr, moe and aux_loss scaling on one gpu

build-nanogpt-master/aae_eval_log_utils.py
@@ -130,7 +130,7 @@ class HellaSwag:
             num_total += 1
             num_correct_norm += int(pred_norm == label)
         # reduce the stats across all processes
-        if self.ddp:
+        if self.fsdp_ddp:
             num_total = torch.tensor(num_total, dtype=torch.long, device=self.device)
             num_correct_norm = torch.tensor(num_correct_norm, dtype=torch.long, device=self.device)
             dist.all_reduce(num_total, op=dist.ReduceOp.SUM)
@@ -154,9 +154,9 @@ class Validation:
     def __init__(self, log_params=None):
         self.model = log_params.model
         self.val_loader = log_params.val_loader
-        self.ddp = log_params.ddp
+        self.fsdp_ddp = log_params.fsdp_ddp
         self.device = log_params.device
-        self.master_process = log_params.ddp_rank == 0
+        self.master_process = log_params.rank == 0
         self.step = log_params.step
         self.shard_idx = log_params.shard_idx
         self.lr = log_params.lr
@@ -180,7 +180,7 @@ class Validation:
                 val_loss = val_loss / val_loss_steps # divide the loss by the number of accumulation steps to get the average loss. This computes the averaage loss on one gpu.
                 val_loss_accum += val_loss.detach() # detach the loss from the computation graph to avoid memory leaks.
 
-        if self.ddp:
+        if self.fsdp_ddp:
             # synchronize the validation loss across all gpu processes
             dist.all_reduce(val_loss_accum, op=dist.ReduceOp.AVG)
             if self.master_process:
@@ -190,7 +190,7 @@ class GenerateSample:
     def __init__(self, log_params):
         self.model = log_params.model
         self.device = log_params.device
-        self.ddp_rank = log_params.ddp_rank
+        self.rank = log_params.rank
         self.enc = log_params.encoder
 
     def generate(self, context="Hello, I'm a language model,", sample_max_length=32):  
@@ -202,7 +202,7 @@ class GenerateSample:
         self.tokens = self.tokens.unsqueeze(0).repeat(self.num_return_sequences, 1)
         self.xgen = self.tokens.to(self.device)
         self.sample_rng = torch.Generator(device=self.device)
-        self.sample_rng.manual_seed(42 + self.ddp_rank)
+        self.sample_rng.manual_seed(42 + self.rank)
 
         while self.xgen.size(1) < sample_max_length:
             # forward the model to get the logits
@@ -227,4 +227,4 @@ class GenerateSample:
         for i in range(self.num_return_sequences):
             self.tokens = self.xgen[i, :sample_max_length].tolist()
             decoded = self.enc.decode(self.tokens)
-            print(f"\nrank {self.ddp_rank} sample {i}: {decoded}\n")
+            print(f"\nrank {self.rank} sample {i}: {decoded}\n")

build-nanogpt-master/aae_model_moe_fsdp.py
@@ -285,8 +285,7 @@ class MoELayerSharded(nn.Module):
                 # final_test = torch.zeros_like(x)
                 # final_test[expert_mask] += expert_output_weighted
                 # print(f'{torch.allclose(final_output, final_test)}')
-
-            break
+             
         
         # each instance
         if self.world_size > 1:
@@ -322,14 +321,14 @@ class Block(nn.Module):
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
         moe_out, aux_loss = self.moe(self.ln_2(x))  
-        x = x + moe_out * self.config.moe_scale
+        x = x + (moe_out * self.moe_scale)
         return x, aux_loss
 
 # %%
 class CreateMoESharded(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.aux_loss_scale = config.aux_loss_scale
+        self.config = config
 
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
@@ -391,13 +390,13 @@ class CreateMoESharded(nn.Module):
         logits = self.lm_head(x) # (B, T, vocab_size)
         
         # if targets are provided, calculate the loss
-        loss = None
+        main_loss = 0
         if targets is not None:
             # Pytorch's cross-entropy loss expects the logits to be of shape (B*T, vocab_size) and the targets to be of shape (B*T). So we need to reshape the logits and targets to match this shape.
             # reshape the logits: (B, T, vocab_size) -> (B*T, vocab_size) to match the shape of the targets: (B, T) -> (B*T) and then calculate the cross-entropy loss
             main_loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
         
-        total_loss = main_loss + (self.aux_loss_scale * aux_loss_total)
+        total_loss = main_loss + (self.config.aux_loss_scale * aux_loss_total)
         
         return logits, total_loss
 