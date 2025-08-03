bug fixing a masking issue on local

build-nanogpt-master/aae_code_test_MoE.py
@@ -308,16 +308,36 @@ print(f"\nSelected cols:\n{selected_cols}")
 
 
 # %%
-expert_id_global = torch.tensor([3, 11, 4, 12])
-top_k_lcl_indices = torch.tensor([2,0])
+D ={
+    '1' : "exper dummy",
+    '3' : "exper dummy",
+    '5' : "exper dummy",
+    '7' : "exper dummy",
+}
+B=3
+T=5
+expert_id_global = torch.tensor([1, 3, 5, 7])
+top_k_lcl_indices = torch.tensor([
+                                [[2,3],[1,0], [3,1], [2,1], [3,0]],
+                                 [[1,2],[1,3], [0,1], [2,0,], [3,2]],
+                                 [[1,3],[2,0], [3,3], [1,0], [0,2]]
+                                 ])
 
 top_k_global_indices = expert_id_global[top_k_lcl_indices]
 
 print(top_k_global_indices)
+# print(top_k_global_indices.view((B*T), -1))
 
-for expert_id_global in 
-expert_mask = (top_k_global_indices == expert_id_global).any(dim=-1)
-print(expert_mask)
+for id_str in D.keys():
+    id = int(id_str)
+    # print(id.shape)
+    print(top_k_global_indices.shape)
+    print(id)
+    expert_mask = (top_k_global_indices == id).any(dim=-1)
+    print(expert_mask)
+    flat_mask = expert_mask.view(-1)
+    print(flat_mask)
+    break
 
 
 

build-nanogpt-master/aae_model_moe_fsdp.py
@@ -164,16 +164,16 @@ class TopKMoEGate(nn.Module):
     def forward(self, x):
         
         # In each batch, there is a logit for each token in the sequence and for each expert. 
-        logits = self.gate_linear(x) # (batch_size, seq_len, num_experts)
+        logits = self.gate_linear(x) # (batch_size, seq_len, num_local_experts)
 
         # The global noise to be added to the logits of each expert. This is a random noise tensor of the same shape as the logits. 
-        noise = torch.randn_like(logits) * self.noisy_std # (batch_size, seq_len, num_experts)
+        noise = torch.randn_like(logits) * self.noisy_std # (batch_size, seq_len, num_local_experts)
 
         # Per-expert noise scaling using self.weights. 
         noise = noise * self.noise_weight
 
         # Add the noise to the logits. 
-        logits_noisy = logits + noise  # (batch_size, seq_len, num_experts)
+        logits_noisy = logits + noise  # (batch_size, seq_len, num_local_experts)
 
         # Get the top-k logits and their corresponding indices. The pytorch top_k method returns the top-k values and their indices along the last dimension (num_experts). In each batch, for each token in the sequence, it selects the top-k logits and their indices from the logits produced by each expert.
         top_k_logits_noisy, top_k_indices_noisy = logits_noisy.topk(self.k, dim=-1)  # (batch_size, seq_len, top_k) 