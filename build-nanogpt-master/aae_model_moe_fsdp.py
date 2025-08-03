bug fix masking issue on 2gpus

build-nanogpt-master/aae_model_moe_fsdp.py
@@ -256,19 +256,20 @@ class MoELayerSharded(nn.Module):
 
             # flatten the mask to match the shape of the flattened input x_flat. Note that the shape of flat_mask is a one dimensional (batch_size*seq_len). x_flat has shape (batch_size * seq_len, n_embd). each row in x_flat is a token in the sequence. 
             flat_mask = expert_mask.view(-1) # (batch_size * seq_len)
-            # print(f'flat_mask shape: {flat_mask.shape} \n{flat_mask}\n')
+            if dist.get_rank() == 0: print(f'flat_mask shape: {flat_mask.shape} \n{flat_mask}\n')
 
             if flat_mask.any():
                 # If the flat mask has any True values, Apply the expert to the inputs selected by the mask. x_flat[flat_mask] picks the rows(tokens) of x_flat where the mask is True. This allows us to activate the expert only for the tokens where the expert with expert_id_global is in the top_k indices.
                 expert_input = x_flat[flat_mask] # (number of tokens where expert_id_global is in top_k, n_embd)
-                # print(f'\nexpert_input shape: {expert_input.shape} \n{expert_input}\n')
+                print(f'\nexpert_input shape: {expert_input.shape} \n{expert_input}\n')
 
                 # apply expert i to the expert_input. Again, note that based on the mask described above, epxert i is applied only to the tokens where it is in the top_k indices.
                 expert_output = self.local_experts[expert_id_global_str](expert_input) # (number of tokens where expert i is in top_k, n_embd)
-                # print(f'expert_output shape: {expert_output.shape} \n{expert_output}\n')
+                if dist.get_rank() == 0: print(f'expert_output shape: {expert_output.shape} \n{expert_output}\n')
 
                 # Now we need to scale the expert output by the gated weights for the tokens where the expert is in the top_k indices. gated_weights_flat has shape (batch_size * seq_len, num_local_experts). We apply the same mask as we used to create expert_input to select all rows from gated_weights_flat where expert i is in the top_k indices, then we select the ith column. This returns the weighting for expert i that is to be applied to the tokens where expert i is in the top_k indices. This is the same as selecting all the non_zero values in the ith column of gated_weights_flat. We  then use unsqueeze(1) to add a dimension to create a column vector of shape (number of tokens where expert i is in top_k, 1). this allows  multiplication with the expert_output which has shape (number of tokens where expert i is in top_k, num_local_experts). 
-                # print(f'top_k_gated_weights_flat shape: {top_k_gated_weights_flat.shape} \n{top_k_gated_weights_flat}\n')
+                print(f'top_k_gated_weights_flat shape: {top_k_gated_weights_flat.shape} \n{top_k_gated_weights_flat}\n')
+                print(f'\nexpert_id_global: {expert_id_global}\n')
 
                 expert_weights = top_k_gated_weights_flat[flat_mask, expert_id_global].unsqueeze(1)  # (number of tokens where expert i is in top_k, 1)
                 # print(f'expert_weights shape: {expert_weights.shape} \n{expert_weights}\n')
@@ -285,7 +286,7 @@ class MoELayerSharded(nn.Module):
                 # final_test = torch.zeros_like(x)
                 # final_test[expert_mask] += expert_output_weighted
                 # print(f'{torch.allclose(final_output, final_test)}')
-             
+            
         
         # each instance
         if self.world_size > 1:

build-nanogpt-master/aae_model_train_moe_fsdp.py
@@ -21,6 +21,7 @@ assert torch.cuda.is_available()  ,"This script is designed to run on CUDA devic
 class GPTConfig:
     seq_len: int = 1024 # max sequence length
     # setting vocab size to 50304 rather than 50257 (the size of the gpt2 vocab) because this is a much more efficient number (divisible by many powers of 2) for gpu kernels and computations. The extra tokens are just padding tokens that are not used in the model. The model will learn to ignore them. this is a tradeoff between memory and performance. 
+    batch_size = 16
     vocab_size: int = 50304
     n_layer: int = 12
     n_head: int = 12
@@ -164,7 +165,7 @@ from aae_dataloader_utils import DataLoaderShardMultiGPU
 # initialize the dataloader for training and validation data. Batch size has to be be customized to fit the gpu being used.
 B = 32 # batch size
 
-train_loader = DataLoaderShardMultiGPU(B=B, seq_len=config.seq_len, process_rank = FSDP_rank, num_processes=FSDP_world_size, split='train')
+train_loader = DataLoaderShardMultiGPU(B=config.batch_size, seq_len=config.seq_len, process_rank = FSDP_rank, num_processes=FSDP_world_size, split='train')
 
 val_loader = DataLoaderShardMultiGPU(B=B, seq_len=config.seq_len, process_rank = FSDP_rank, num_processes=FSDP_world_size, split='val')
 